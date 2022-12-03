4dd2fb6689a858651cf6d91161815b6141b1e447
jokereven <zjing125.cn@gmail.com>
2022-12-03 22:29:53 +0800

    0x5FbDB2315678afecb367f032d93F642f64180aa3

--- hardhat-tutorial/constants/index.ts
+++ hardhat-tutorial/constants/index.ts
@@ -1,2 +1,2 @@
 export const WHITELIST_CONTRACT_ADDRESS = "0x4aA717ef6C31fd152aa5C4d2601eC22C27Da2b78";
-export const METADATA_URL = "https://nft-collection-mitsuki.vercel.app/api/";
+export const METADATA_URL = "https://nft-collection-pi-rust.vercel.app/api/";
